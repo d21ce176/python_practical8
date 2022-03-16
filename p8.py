75296cd210e41cefdd9f4e62186dbe7de1faa6ec
d21ce176 <97421522+d21ce176@users.noreply.github.com>
2022-03-16 17:11:42 +0530

    Add files via upload

---
+++ p8.py
@@ -0,0 +1,18 @@
+stack=[]
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print ('install stack')
+
+print (stack)
+
+print ('\n element pop from stack')
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+
+print ('\n stack after element are poped:')
+print(stack)
+
+print("this program is prepared by om and id :d21ce176")  
